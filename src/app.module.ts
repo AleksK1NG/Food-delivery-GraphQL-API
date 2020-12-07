003438be18343b088d9857fedb9d20f172fd06c3
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-07 19:28:39 +0300

    add typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'eats_db',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       debug: true,
       playground: true,
