45e74ded909002e94dc28faba358af3c498b4d8c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-07 19:15:56 +0300

    add CreateRestaurantDto validation

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,16 +1,22 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
+  @IsString()
   @Field(() => String)
   name: string;
 
+  @IsBoolean()
   @Field(() => Boolean)
   isVegan: boolean;
 
+  @IsString()
   @Field(() => String)
   address: string;
 
+  @Length(5, 20)
+  @IsString()
   @Field(() => String)
   ownerName: string;
 }
