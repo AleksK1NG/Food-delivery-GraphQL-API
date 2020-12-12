d90aba7592221ab99ea4576c74470577a4293a59
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-12 17:05:48 +0300

    feat: add user compare password method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateAccountInput, CreateAccountOutput } from './dto/createAccount.dto';
+import { LoginInput, LoginOutput } from './dto/login.dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
 
   async createAccount(createAccountInput: CreateAccountInput): Promise<CreateAccountOutput> {
-    const { email, password, role } = createAccountInput;
-    try {
-      const exists = await this.usersRepository.findOne({ email });
-      if (exists) return { ok: false, error: 'There is a user with that email already' };
+    const { email } = createAccountInput;
+    const exists = await this.usersRepository.findOne({ email });
+    if (exists) throw new Error(`E-mail ${email} is already in use.`);
 
-      const user = this.usersRepository.create({ ...createAccountInput });
-      await this.usersRepository.save(user);
-      return { ok: true, error: null };
-    } catch (e) {
-      return { ok: false, error: e.message };
-    }
+    await this.usersRepository.create({ ...createAccountInput });
+    return { ok: true };
+  }
+
+  async login(loginInput: LoginInput): Promise<LoginOutput> {
+    const { email, password } = loginInput;
+    const user = await this.usersRepository.findOne({ email });
+    if (!user) throw new NotFoundException(`User with email: ${email} not found`);
+
+    const isPasswordValid = await user.checkPassword(password);
+    if (!isPasswordValid) throw new UnauthorizedException('Invalid email or password');
+
+    return { ok: true, token: 'adasdsadads' };
   }
 }
