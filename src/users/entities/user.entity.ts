ff9e695051fe18c466930bf74d0176846b6fd54c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-12 15:51:53 +0300

    feat: add create account BeforeInsert hashPassword

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
 import { IsEmail, IsString } from 'class-validator';
 import { CoreEntity } from '../../common/entities/core.entity';
 import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
+import * as bcrypt from 'bcrypt';
 
 enum UserRole {
   Owner,
@@ -28,4 +29,9 @@ export class User extends CoreEntity {
   @Column({ type: 'enum', enum: UserRole })
   @Field(() => UserRole)
   role: UserRole;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
 }
