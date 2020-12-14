1c9e13aeaeebb84b3143f57a7335619a1ce3ae10
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-14 13:17:02 +0300

    feat: add category and restaurants entities

---
+++ src/restaurants/entities/cetegory.entity.ts
@@ -0,0 +1,30 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { CoreEntity } from '../../common/entities/core.entity';
+import { IsString, Length } from 'class-validator';
+import { Restaurant } from './restaurant.entity';
+
+@InputType('CategoryInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Category extends CoreEntity {
+  @Field(() => String)
+  @Column({ unique: true })
+  @IsString()
+  @Length(5)
+  name: string;
+
+  @Field(() => String, { nullable: true })
+  @Column({ nullable: true })
+  @IsString()
+  coverImg: string;
+
+  @Field(() => String)
+  @Column({ unique: true })
+  @IsString()
+  slug: string;
+
+  @Field(() => [Restaurant], { nullable: true })
+  @OneToMany(() => Restaurant, (restaurant) => restaurant.category)
+  restaurants: Restaurant[];
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,10 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
-import { IsEmail, IsString } from 'class-validator';
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany } from 'typeorm';
+import { IsBoolean, IsEmail, IsString } from 'class-validator';
 import { CoreEntity } from '../../common/entities/core.entity';
 import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
 import * as bcrypt from 'bcrypt';
 import { Exclude } from 'class-transformer';
+import { Restaurant } from '../../restaurants/entities/restaurant.entity';
 
 enum UserRole {
   Owner,
@@ -13,11 +14,11 @@ enum UserRole {
 
 registerEnumType(UserRole, { name: 'UserRole' });
 
-@InputType({ isAbstract: true })
+@InputType('UserInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
 export class User extends CoreEntity {
-  @Column()
+  @Column({ unique: true })
   @IsEmail()
   @IsString()
   @Field(() => String)
@@ -34,7 +35,12 @@ export class User extends CoreEntity {
 
   @Column({ default: false })
   @Field(() => Boolean)
+  @IsBoolean()
   isVerified: boolean;
+
+  @Field(() => [Restaurant])
+  @OneToMany(() => Restaurant, (restaurant) => restaurant.owner)
+  restaurants: Restaurant[];
 
   @BeforeInsert()
   @BeforeUpdate()
