ae01eedf4872d5e104ccd3480fd3b6b0495e024d
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-15 15:06:12 +0300

    feat: add orders crud resolvers

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,10 +1,13 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Order } from './entities/order.entity';
 import { OrdersService } from './orders.service';
 import { CreateOrderInput, CreateOrderOutput } from './dto/create-order.dto';
 import { Roles } from '../auth/decorators/role.decorator';
 import { AuthUser } from '../auth/decorators/auth-user.decorator';
 import { User } from '../users/entities/user.entity';
+import { GetOrdersInput, GetOrdersOutput } from './dto/get-orders.dto';
+import { GetOrderInput, GetOrderOutput } from './dto/get-order.dto';
+import { EditOrderInput, EditOrderOutput } from './dto/edit-order.dto';
 
 @Resolver(() => Order)
 export class OrdersResolver {
@@ -18,5 +21,23 @@ export class OrdersResolver {
     createOrderInput: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     return this.ordersService.crateOrder(customer, createOrderInput);
+  }
+
+  @Query(() => GetOrdersOutput)
+  @Roles(['Any'])
+  async getOrders(@AuthUser() user: User, @Args('input') getOrdersInput: GetOrdersInput): Promise<GetOrdersOutput> {
+    return this.ordersService.getOrders(user, getOrdersInput);
+  }
+
+  @Query(() => GetOrderOutput)
+  @Roles(['Any'])
+  async getOrder(@AuthUser() user: User, @Args('input') getOrderInput: GetOrderInput): Promise<GetOrderOutput> {
+    return this.ordersService.getOrder(user, getOrderInput);
+  }
+
+  @Mutation(() => EditOrderOutput)
+  @Roles(['Any'])
+  async editOrder(@AuthUser() user: User, @Args('input') editOrderInput: EditOrderInput): Promise<EditOrderOutput> {
+    return this.ordersService.editOrder(user, editOrderInput);
   }
 }
