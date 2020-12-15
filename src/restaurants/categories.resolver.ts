5165c10d8ebe9dc96ca087c8c620507b3bbb7453
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-15 10:39:38 +0300

    feat: add restaurants module category resolver

---
+++ src/restaurants/categories.resolver.ts
@@ -0,0 +1,25 @@
+import { Args, Int, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { Category } from './entities/cetegory.entity';
+import { AllCategoriesOutput } from './dto/all-categories.dto';
+import { RestaurantsService } from './restaurants.service';
+import { CategoryInput, CategoryOutput } from './dto/category.dto';
+
+@Resolver(() => Category)
+export class CategoryResolver {
+  constructor(private readonly restaurantsService: RestaurantsService) {}
+
+  @ResolveField(() => Int)
+  restaurantCount(@Parent() category: Category): Promise<number> {
+    return this.restaurantsService.countRestaurants(category);
+  }
+
+  @Query(() => AllCategoriesOutput)
+  async allCategories(): Promise<AllCategoriesOutput> {
+    return this.restaurantsService.allCategories();
+  }
+
+  @Query(() => CategoryOutput)
+  category(@Args() categoryInput: CategoryInput): Promise<CategoryOutput> {
+    return this.restaurantsService.findCategoryBySlug(categoryInput);
+  }
+}
--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantsService } from './restaurants.service';
 import { CategoryRepository } from './repositories/category.repository';
+import { CategoryResolver } from './categories.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
-  providers: [RestaurantsResolver, RestaurantsService],
+  providers: [RestaurantsResolver, RestaurantsService, CategoryResolver],
 })
 export class RestaurantsModule {}
