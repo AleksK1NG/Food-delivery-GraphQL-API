5a61caf5023271fc4c574e3da94c79b269f528c5
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-15 14:46:55 +0300

    feat: add orders service getOrders method

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -5,8 +5,9 @@ import { Repository } from 'typeorm';
 import { OrderItem } from './entities/order-item.entity';
 import { Restaurant } from '../restaurants/entities/restaurant.entity';
 import { Dish } from '../restaurants/entities/dish.entity';
-import { User } from '../users/entities/user.entity';
+import { User, UserRole } from '../users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dto/create-order.dto';
+import { GetOrdersInput, GetOrdersOutput } from './dto/get-orders.dto';
 
 @Injectable()
 export class OrdersService {
@@ -73,4 +74,54 @@ export class OrdersService {
 
     return { ok: true };
   }
+
+  async getOrders(user: User, input: GetOrdersInput): Promise<GetOrdersOutput> {
+    const { status } = input;
+
+    let orders: Order[];
+    if (user.role === UserRole.Client) {
+      orders = await this.ordersRepository.find({
+        where: {
+          customer: user,
+          ...(status && { status }),
+        },
+      });
+    } else if (user.role === UserRole.Delivery) {
+      orders = await this.ordersRepository.find({
+        where: {
+          driver: user,
+          ...(status && { status }),
+        },
+      });
+    } else if (user.role === UserRole.Owner) {
+      const restaurants = await this.restaurantsRepository.find({
+        where: {
+          owner: user,
+        },
+        relations: ['orders'],
+      });
+      orders = restaurants.map((restaurant) => restaurant.orders).flat(1);
+      if (status) {
+        orders = orders.filter((order) => order.status === status);
+      }
+    }
+
+    return { ok: true, orders };
+  }
+
+  canSeeOrder(user: User, order: Order): boolean {
+    let canSee = true;
+    if (user.role === UserRole.Client && order.customerId !== user.id) {
+      canSee = false;
+    }
+    if (user.role === UserRole.Delivery && order.driverId !== user.id) {
+      canSee = false;
+    }
+    if (user.role === UserRole.Owner && order.restaurant.ownerId !== user.id) {
+      canSee = false;
+    }
+    return canSee;
+  }
+
+
 }
